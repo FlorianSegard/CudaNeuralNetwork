53f72f29ca861d6bacf70321dc880934af780143
FlorianBGdu75 <florian.segard-gahery@epita.fr>
2024-12-07 22:34:26 +0100

    commebts

--- Model/Model.hpp
+++ Model/Model.hpp
@@ -35,30 +35,30 @@ struct Model
 };
 
 
-struct LayerParams {
-    Tensor<float> weights;
-    Tensor<float> biases;
-    Tensor<float> dWeights;   // Gradients for backward
-    Tensor<float> dBiases;    // Gradients for backward
-    int inputSize = 0;
-    int outputSize = 0;
+// struct LayerParams {
+//     Tensor<float> weights;
+//     Tensor<float> biases;
+//     Tensor<float> dWeights;   // Gradients for backward
+//     Tensor<float> dBiases;    // Gradients for backward
+//     int inputSize = 0;
+//     int outputSize = 0;
 
-    LayerParams() = default; // Default constructor
-    LayerParams(int inputSize, int outputSize, bool device)
-        : weights(inputSize, outputSize, device),
-          biases(1, outputSize, device),
-          dWeights(inputSize, outputSize, device),
-          dBiases(1, outputSize, device),
-          inputSize(inputSize), outputSize(outputSize) {}
+//     LayerParams() = default; // Default constructor
+//     LayerParams(int inputSize, int outputSize, bool device)
+//         : weights(inputSize, outputSize, device),
+//           biases(1, outputSize, device),
+//           dWeights(inputSize, outputSize, device),
+//           dBiases(1, outputSize, device),
+//           inputSize(inputSize), outputSize(outputSize) {}
 
 
-    // TODO in Tensor.hpp
-    void switchDevice(bool device) {
-        // Logic to reallocate or transfer data between CPU and GPU
-        weights.switchDevice(device);
-        biases.switchDevice(device);
-        dWeights.switchDevice(device);
-        dBiases.switchDevice(device);
-    }
+//     // TODO in Tensor.hpp
+//     void switchDevice(bool device) {
+//         // Logic to reallocate or transfer data between CPU and GPU
+//         weights.switchDevice(device);
+//         biases.switchDevice(device);
+//         dWeights.switchDevice(device);
+//         dBiases.switchDevice(device);
+//     }
 
-};
+// };
