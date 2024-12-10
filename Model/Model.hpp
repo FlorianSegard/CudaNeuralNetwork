02103efe1e5bf62125cf03dffc57e988aa1068a3
Florian <floriansegard@gmail.com>
2024-12-10 23:06:18 +0100

    model with layers as unique ptr

--- Model/Model.hpp
+++ Model/Model.hpp
@@ -13,39 +13,36 @@
 
 struct Model
 {
-    std::vector<Layer> layers;
+    std::vector<std::unique_ptr<Layer>> layers;
     SGD optimizer;
 
-    Model() : layers(), optimizer(0.001f) {}
+    Model() : optimizer(0.001f) {}
 
-    explicit Model(std::vector<Layer> other_layers)
-        : layers(std::move(other_layers)), optimizer(0.001f) {}
+    explicit Model(std::vector<std::unique_ptr<Layer>> other_layers)
+            : layers(std::move(other_layers)), optimizer(0.001f) {}
 
-    Model(std::vector<Layer>& other_layers, SGD opt)
-        : layers(std::move(other_layers)), optimizer(std::move(opt)) {}
+    Model(std::vector<std::unique_ptr<Layer>>& other_layers, SGD opt)
+            : layers(std::move(other_layers)), optimizer(std::move(opt)) {}
 
-    void addLayer(Layer& layer)
-    {
-        layers.push_back(layer);
+    void addLayer(std::unique_ptr<Layer> layer) {
+        layers.push_back(std::move(layer));
     }
 
     void setOptimizer(const SGD& opt) {
         optimizer = opt;
     }
 
-    Tensor<float> forward(Tensor<float> input) 
-    {
+    Tensor<float> forward(Tensor<float> input) {
         for (auto& layer : layers) {
-            input = layer.forward(input);
+            input = layer->forward(input);
         }
         return input;
     }
 
-    void backward(Tensor<float> dOutput)
-    {
+    void backward(Tensor<float> dOutput) {
         Tensor<float> dInput = dOutput;
         for (auto it = layers.rbegin(); it != layers.rend(); ++it) {
-            dInput = it->backward(dInput);
+            dInput = (*it)->backward(dInput);
         }
     }
 
@@ -58,31 +55,3 @@ struct Model
     }
 };
 
-
-// struct LayerParams {
-//     Tensor<float> weights;
-//     Tensor<float> biases;
-//     Tensor<float> dWeights;   // Gradients for backward
-//     Tensor<float> dBiases;    // Gradients for backward
-//     int inputSize = 0;
-//     int outputSize = 0;
-
-//     LayerParams() = default; // Default constructor
-//     LayerParams(int inputSize, int outputSize, bool device)
-//         : weights(inputSize, outputSize, device),
-//           biases(1, outputSize, device),
-//           dWeights(inputSize, outputSize, device),
-//           dBiases(1, outputSize, device),
-//           inputSize(inputSize), outputSize(outputSize) {}
-
-
-//     // TODO in Tensor.hpp
-//     void switchDevice(bool device) {
-//         // Logic to reallocate or transfer data between CPU and GPU
-//         weights.switchDevice(device);
-//         biases.switchDevice(device);
-//         dWeights.switchDevice(device);
-//         dBiases.switchDevice(device);
-//     }
-
-// };
