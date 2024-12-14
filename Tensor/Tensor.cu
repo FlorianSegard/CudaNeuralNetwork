80d4bd45b6cfedabaa81ae494661ee0510882759
FlorianBGdu75 <florian.segard-gahery@epita.fr>
2024-12-14 18:42:43 +0100

    [Tensor] dot fixed error gridsize

--- Tensor/Tensor.cu
+++ Tensor/Tensor.cu
@@ -80,17 +80,19 @@ template void fillZeroGPU(Tensor<int>& input);
 //     int x = blockIdx.x * blockDim.x + threadIdx.x;
 //     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-//     if (y < height_input && x < width_output) {
+//     if (y < height_input && x < width_output) 
+//     {
 //         T sum = 0;
-//         for (int k = 0; k < width_input; k++)
+//         for (int k = 0; k < width_input; k++) 
 //         {
 //             T a_val = input[k + y * inputStride / sizeof(T)];
 //             T b_val = other[x + k * otherStride / sizeof(T)];
 //             sum += a_val * b_val;
 //         }
 //         result[x + y * resultStride / sizeof(T)] = sum;
-//     }
+//      }
 // }
+
 
 
 #define TILE_SIZE 16
@@ -148,7 +150,7 @@ __global__ void dotGPUKernel(T* input, T* other, T* result,
 template <class T>
 Tensor<T> dotGPU(const Tensor<T>& input, const Tensor<T>& other) {
     dim3 blockSize(16, 16);
-    dim3 gridSize((input.width + blockSize.x - 1) / blockSize.x,
+    dim3 gridSize((other.width + blockSize.x - 1) / blockSize.x,
                   (input.height + blockSize.y - 1) / blockSize.y);
 
     Tensor<T> result(other.width, input.height, true);
@@ -255,7 +257,8 @@ template Tensor<int> addGPU(const Tensor<int>& input, const Tensor<int>& other);
 // ----------------------------------------------------------- Scalar Mult ----------------------------------------------------------- \\
 
 template <class T>
-__global__ void scalarMultiplyKernel(const T* input, T* output, T scalar, int width, int height, size_t inStride, size_t outStride) {    int x = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void scalarMultiplyKernel(const T* input, T* output, T scalar, int width, int height, size_t inStride, size_t outStride) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (x < width && y < height) {
