a856fde85648d881ef58f6278370d07be199923c
Alex DR <alexandre.devaux-riviere@epita.fr>
2024-12-17 17:15:28 +0100

    [main] fix debug call in softmax layer

--- Layers/Softmax/Softmax.hpp
+++ Layers/Softmax/Softmax.hpp
@@ -22,7 +22,7 @@ struct Softmax : public Layer {
 
     // Forward pass for Softmax
     Tensor<float> computeForward(Tensor<float>& input) override {
-        Logger::infer(">>> Linear");
+        Logger::infer(">>> Softmax");
         if (input.device == true) {
             return softmaxGPU(input);
         } else {
